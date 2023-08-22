40f5338c2e2bcc20f9a1d581bccce73104c74b6c
SamuelBAlba <brayanbalba24@gmail.com>
2023-08-22 19:39:21 -0400

    tables created

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS carevillage_dev;
 
 CREATE DATABASE carevillage_dev;
 
-\c carevillage_d
+\c carevillage_dev
 
 
 CREATE TABLE user (
@@ -12,3 +12,57 @@ CREATE TABLE user (
     address VARCHAR NOT NULL,
     email VARCHAR NOT NULL
 );
+
+DROP TABLE IF EXISTS forums;
+
+CREATE TABLE forums (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERNCES user (id),
+    title VARCHAR NOT NULL,
+    content VARCHAR NOT NULL,
+    date VARCHAR NOT NULL,
+    category VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS comment;
+
+CREATE TABLE comment (
+    id SERIAL PRIMARY KEY,
+    post_id INTEGER REFERNCES forums (id),
+    user_id INTEGER REFERNCES user (id),
+    content VARCHAR NOT NULL,
+    date VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS donations;
+
+CREATE TABLE donations (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERNCES user (id),
+    category VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    date VARCHAR NOT NULL,
+    img VARCHAR
+);
+
+DROP TABLE IF EXISTS donations_comment;
+
+CREATE TABLE donations_comment (
+    id PRIMARY SERIAL KEY,
+    donation_post_id INTEGER REFERNCES donations (id),
+    user_id INTEGER REFERNCES user (id),
+    content VARCHAR NOT NULL,
+    date VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS likes;
+
+CREATE TABLE likes (
+    post_id INTEGER REFERNCES forums (id),
+    user_id INTEGER REFERNCES user (id)
+);
+
+
+
+
