ab463040f24c53c6d3d94e6d46c6a5f7de2e48aa
SamuelBAlba <brayanbalba24@gmail.com>
2023-08-21 21:07:08 -0400

    adding more files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+
+
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to CareVillage");
+})
+
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page Not Found" });
+})
+
+
+module.exports = app;
---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS carevillage_dev;
+
+CREATE DATABASE carevillage_dev;
+
+\c carevillage_dev
