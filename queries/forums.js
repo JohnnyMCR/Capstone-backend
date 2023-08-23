c51e7f9ab78bbb18aef638725c3c2d111e5f1f35
MaxWattan <mcwattanac@gmail.com>
2023-08-22 20:58:55 -0400

    removed line 51

--- queries/forums.js
+++ queries/forums.js
@@ -48,7 +48,6 @@ const DELETE_FORUM = async (id) => {
 //update forum
 const UPDATE_FORUM = async (id, forum) => {
     try {
-        user_id, title, content, date, category
         const UPDATED_FORUM = await db.one(`UPDATE forums SET user_id=$1, title=$2, content=$3, date=$4, category=$5, WHERE id=$6 RETURNING *`, [forum.user_id, forum.title, donation.content, forum.date, forum.category, id])
 
         return UPDATED_FORUM
