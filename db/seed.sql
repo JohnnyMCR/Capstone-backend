13fd0623f73fc2b65f41861d8ea1ce2a8386a25d
Angel <EMAIL>
2023-10-05 21:59:38 -0400

    backend updated

--- db/seed.sql
+++ db/seed.sql
@@ -21,10 +21,12 @@ VALUES
   ('Newborn'),
   ('Expecting');
 
-INSERT INTO donations (user_id, category, title, description, date)
+
+INSERT INTO donations (user_id, category, title, description, date, img)
 VALUES
-  (1, 'Charity', 'Supporting Local Baby Supplies Drive', 'Donating baby supplies for newborns in need.', '2023-08-10'),
-  (2, 'Education', 'Donating Parenting Books', 'Contributing parenting books for expecting parents.', '2023-08-15');
+  (1, 'Charity', 'Supporting Local Baby Supplies Drive', 'Donating baby supplies for newborns in need.', '2023-08-10', ''),
+  (2, 'Education', 'Donating Parenting Books', 'Contributing parenting books for expecting parents.', '2023-08-15', '');
+
 
 INSERT INTO donations_comments (donations_id, user_id, content, date)
 VALUES
