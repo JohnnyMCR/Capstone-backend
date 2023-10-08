49389d209ba22ebcf0597e416919ed76b9907768
MaxWattan <mcwattanac@gmail.com>
2023-10-08 18:47:46 -0400

    added some category pictures to seed

--- db/seed.sql
+++ db/seed.sql
@@ -21,24 +21,28 @@ VALUES
 
 INSERT INTO categories (name)
 VALUES
-  ('Newborn'),
-  ('Expecting'),
-  ('Charity'),
-  ('Parenting');
+  ('Clothing'),
+  ('Toys'),
+  ('Food'),
+  ('Educational'),
+  ('Other');
 
 INSERT INTO donations (user_id, category, title, description, date, img)
 VALUES
-  (4, 'Charity', 'Donating Baby Swing for Infants With Bluetooth Music Speaker', 'I am Donating Dream on me Zazu Motorized Baby Swing for Infants - Bluetooth Music Speaker, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_9bb73436-6c64-4885-95ea-2d7731ddb937?wid=600&hei=600&qlt=80&fmt=webp'),
-   (2, 'Charity', 'Donating Rocker Chair', 'I am Donating a Delta Children Kenwood Slim Nursery Glider Swivel Rocker Chair - Sea Breeze, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_673c767c-ba80-47d1-8c3e-d2fbe912466e?wid=1200&hei=1200&qlt=80&fmt=webp'),
-    (3, 'Charity', 'Donating 5-in1 Humidifier/Sound Machine', 'I am Donating Baby Dream Machine 5-in-1 Childrens Sleep Device
+  (4, 'Other', 'Donating Baby Swing for Infants With Bluetooth Music Speaker', 'I am Donating Dream on me Zazu Motorized Baby Swing for Infants - Bluetooth Music Speaker, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_9bb73436-6c64-4885-95ea-2d7731ddb937?wid=600&hei=600&qlt=80&fmt=webp'),
+   (1, 'Educational', 'Donating Parenting Book', 'I am Donating Peaceful Parent, Happy Kids - by Laura Markham, It is a good read, in good condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_474a1a64-5f33-4593-9990-bd5fba7ef38b?wid=1200&hei=1200&qlt=80&fmt=webp'),
+ (4, 'Clothing', 'Donating Baby Clothes 0-3Months', 'I am Donating Baby 5pk Sesame Street Elmo/CookieMonster/Oscar the Grouch/Big Bird Bodysuit - Red/Yellow/Blue, They are new.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_dfd1aff9-cc88-42e1-ac80-27457760152d?wid=1200&hei=1200&qlt=80&fmt=webp'),
+
+   (2, 'Other', 'Donating Rocker Chair', 'I am Donating a Delta Children Kenwood Slim Nursery Glider Swivel Rocker Chair - Sea Breeze, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_673c767c-ba80-47d1-8c3e-d2fbe912466e?wid=1200&hei=1200&qlt=80&fmt=webp'),
+    (3, 'Other', 'Donating 5-in1 Humidifier/Sound Machine', 'I am Donating Baby Dream Machine 5-in-1 Childrens Sleep Device
 , Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_3a8ad9a9-9667-4a12-88d3-19d03791a9da?wid=1200&hei=1200&qlt=80&fmt=webp'),
-     (1, 'Charity', 'Donating Baby Activity Center', 'I am Donating Ingenuity Spring & Sprout 2-in-1 Baby Activity Center - First Forest, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_ac494bda-7795-48c7-aac8-16f6d074256c?wid=1200&hei=1200&qlt=80&fmt=webp'),
+     (1, 'Toys', 'Donating Baby Activity Center', 'I am Donating Ingenuity Spring & Sprout 2-in-1 Baby Activity Center - First Forest, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_ac494bda-7795-48c7-aac8-16f6d074256c?wid=1200&hei=1200&qlt=80&fmt=webp'),
 
-     (1, 'Charity', 'Donating Stroller Wagon', 'I am Donating Jeep Wrangler Stroller Wagon with Included Car Seat Adapter by Delta Children - Gray, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_32692769-b2a9-453c-9637-ead46ff76249?wid=1200&hei=1200&qlt=80&fmt=webp'),
-     (1, 'Charity', 'Donating Baby Carrier', 'I am Donating LILLEbaby Baby Carrier SeatMe All Seasons, Still like new condition. ', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_6dbec8f8-ab34-4bdc-a885-b325af0d403d?wid=1200&hei=1200&qlt=80&fmt=webp'),
-     (1, 'Charity', 'Donating Twin Nursery', 'I am Donating Baby Trend Retreat Twins Nursery Center - Quarry, Still like new condition. ', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_ca4db6c1-bfe0-440f-9167-8bf03e9d851e?wid=1200&hei=1200&qlt=80&fmt=webp'),
+     (2, 'Other', 'Donating Stroller Wagon', 'I am Donating Jeep Wrangler Stroller Wagon with Included Car Seat Adapter by Delta Children - Gray, Still like new condition.', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_32692769-b2a9-453c-9637-ead46ff76249?wid=1200&hei=1200&qlt=80&fmt=webp'),
+     (1, 'Other', 'Donating Baby Carrier', 'I am Donating LILLEbaby Baby Carrier SeatMe All Seasons, Still like new condition. ', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_6dbec8f8-ab34-4bdc-a885-b325af0d403d?wid=1200&hei=1200&qlt=80&fmt=webp'),
+     (4, 'Other', 'Donating Twin Nursery', 'I am Donating Baby Trend Retreat Twins Nursery Center - Quarry, Still like new condition. ', '2023-08-10', 'https://target.scene7.com/is/image/Target/GUEST_ca4db6c1-bfe0-440f-9167-8bf03e9d851e?wid=1200&hei=1200&qlt=80&fmt=webp'),
 
-  (3, 'Charity', 'Donating Baby Monitor w/2 Cameras', 'Contributing Motorola 5" WiFi HD Video Baby Monitor w/2 Cameras & PTZ - VM65-2CONNECT, It is working and the condition is like new.', '2023-08-15', 'https://target.scene7.com/is/image/Target/GUEST_8ee71c9f-98c3-40cf-8ad2-eae8c350b39c?wid=1200&hei=1200&qlt=80&fmt=webp');
+  (3, 'Other', 'Donating Baby Monitor w/2 Cameras', 'Contributing Motorola 5" WiFi HD Video Baby Monitor w/2 Cameras & PTZ - VM65-2CONNECT, It is working and the condition is like new.', '2023-08-15', 'https://target.scene7.com/is/image/Target/GUEST_8ee71c9f-98c3-40cf-8ad2-eae8c350b39c?wid=1200&hei=1200&qlt=80&fmt=webp');
 
 INSERT INTO donations_comments (donations_id, user_id, content, date)
 VALUES
